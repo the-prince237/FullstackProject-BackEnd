270b26ea0d067f27ef585f576c61b216afcd4fa2
Tasondock Durin <temgoua484@gmail.com>
2021-08-30 16:28:46 +0100

    - on télécharge express dans notre dossier backend afin de créer plus facilement notre application node; - on crée un fichier app.js qui va abriter notre application; - on l'exporte dans notre fichier sever.Js afin qu'il puisse être exécuté; - on ajoute des middleware, qui sont des bouts de code dans notre application ; - les middleware sont pourvus de 3 arguments, req, res et next. la fonction next() permet de dire à l'application de passer au bout de code (middleware) suivant; - enfin, dans notre fichier server.js, on configure le fichier importé app.js afin qu'il puisse être exécuté via le port désiré, puis on y fait appel dans la constante abritant le serveur http.

---
+++ app.js
@@ -0,0 +1,28 @@
+//nous importons le package express après avoir installé express à partir de notre dossier backend. 
+const express = require("express");
+
+
+const app = express();//nous créons une application express grace à la méthode express()
+
+//nous créons des middlewares, affectés des arguments req, res, et next.
+// ici, la fonction next permet de passer au middleware suivant après avoir éxécuté celui en cours 
+app.use((req, res, next) => {
+    console.("requête reçue !");
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json({message : "votre requête a bien été reçue !"});
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("reponse envoyée avec succès !");
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,19 +1,7 @@
-//importons le package HTTP natif de node quin nous permettra de créer le serveur
 const http = require("http");
+const app = require("./app");
 
-/* créons le serveur avec la methode "createServer" sur http. 
-On se sert d'une fonction qui prend en parameètre les objets request et resopnse.
-enfin , la methode end de la réponse nous permet de renvoyer un résultat de type string
-*/
-const server = http.createServer((req, res) => {
-    res.end("voilà la réponse du serveur!");
-});
+app.set("port", process.env.PORT || 3000);
+const server = http.createServer(app);
 
-/*enfin, on configure le serveur pour qu'il écoute
-- Soit la variable d'environnement du port grace à "process.env.PORT", c'est celui-ci qu'on écoutera
-- Soit le port 3000, ce qui nous servira dans le cas de notre plateforme de développement*/
 server.listen(process.env.PORT || 3000);
-
-//installer nodemon avec la commande "npm install -g nodemon"
-//utiliser la commande "nodemon server" pour écouter que le serveur écoute le port paramétré
-//lancer la page "http://localhost:3000" dans le navigateur.
