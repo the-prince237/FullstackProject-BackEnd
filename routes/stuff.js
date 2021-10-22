054050396200ec69810c45c4245a6c9d5fe8d0e4
Tasondock Durin <temgoua484@gmail.com>
2021-10-22 13:51:37 +0100

    Authentification par création des token : - pour commencer, on installe dans notre dossier backend le package jsonwebtoken qui nous permet de créer et de gérer les token; - ensuite, dans notre fichier user.js (controllers), dans la fonction exportée login, on modifie la valeur de token par le package jwt (nom donné à jsonwebtoken lors de son importation, suivie de sa méthode sign contenant  3 paramètres : * l'id de l'utilisateur, le token, et le delai d'expiration({userId: user._id}, 'RANDOM_TOKEN_SECRET', { expiresIn: '24h' }); - ensuite, on crée un dossier middleware, dans lequel on crée un fichier auth.js, qui portera le middleware de vérification des token. L'idée est de se rassurer que pour une action donnée effectuée par l'utilisateur, la réponse envoyée correspond bien à l'id de l'utilisateur et au token généré pour celui ci; - enfin, dans le fichier stuff.js(routes), on importe le fichier auth.js, et on  ajoute en deuxième paramètre des routes de notre api le nom d'importation de ce fichier. A partir de là, notre API est vraiment sécurisée.

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,13 +1,14 @@
 const express = require("express");
 
 const stuffCtrl = require("../controllers/stuff");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post("/", stuffCtrl.createThing);
-router.get("/:id", stuffCtrl.getOneThing);
-router.put("/:id", stuffCtrl.modifyThing);
-router.delete("/:id", stuffCtrl.deleteThing);
-router.get("/", stuffCtrl.getAllStuff);
+router.post("/", auth, stuffCtrl.createThing);
+router.get("/:id", auth, stuffCtrl.getOneThing);
+router.put("/:id", auth, stuffCtrl.modifyThing);
+router.delete("/:id", auth, stuffCtrl.deleteThing);
+router.get("/", auth, stuffCtrl.getAllStuff);
 
 module.exports = router;
