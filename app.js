c73d62cbd10efba7ba2a9cae3419ef45d88153f7
Tasondock Durin <temgoua484@gmail.com>
2021-10-23 18:45:54 +0100

    terminé

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const stuffRoutes = require("./routes/stuff");
 const userRoutes = require("./routes/user");
@@ -25,5 +26,6 @@ app.use(bodyParser.json());
 
 app.use("/api/stuff", stuffRoutes);
 app.use("/api/auth", userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
